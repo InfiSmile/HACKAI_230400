f0fd46aece2eadbdd44385f10f8b4bee1c5ac111
InfiSmile <104770493+InfiSmile@users.noreply.github.com>
2023-10-11 00:01:00 +0530

    Update alert_request.py

--- hackai_env/alert_request.py
+++ hackai_env/alert_request.py
@@ -1,35 +1,30 @@
 from uagents import Context, Model, Protocol
 
-
+# Define a model for fetching temperature and specifying temperature limits
 class fetch_temp(Model):
-    current_temp:float
+    current_temp: float
     temperature_min: float
-    temperature_max:float
-    
+    temperature_max: float
+
+# Define a model for the alert response
 class AlertResponse(Model):
-    
     alert_status: str
 
-# 
+# Create a Protocol for alert-related communication
+alert_proto = Protocol()
 
-alert_proto= Protocol()
-
-
-# @alert_proto.on_message(model=fetch_temp)
-# async def check_temperature_alert( ctx: Context,sender:str,msg:fetch_temp):
-#     print(f"hey: {msg.current_temp}")
-# f"Temperature is below the preferred range {minimum}°C."
-@alert_proto.on_message(model=fetch_temp,replies=AlertResponse)
-async def check_temperature_alert( ctx: Context,sender:str,msg:fetch_temp):
-        #ctx.logger.info(f"Received message from {sender}: {msg.current_temp}")
-        minimum = msg.temperature_min
-        maximum = msg.temperature_max
-        # print(minimum)
-        if msg.current_temp < minimum:
-            
-            await ctx.send(sender,AlertResponse(alert_status=f"Temperature is below the preferred range {minimum}°C."))
-            
-        if msg.current_temp > maximum:
-
-            await ctx.send(sender,AlertResponse(alert_status=f"Temperature is above the preferred range {maximum}°C."))
-            
+# Define a message handler to check temperature and send alerts
+@alert_proto.on_message(model=fetch_temp, replies=AlertResponse)
+async def check_temperature_alert(ctx: Context, sender: str, msg: fetch_temp):
+    # Get temperature and limits from the message
+    current_temperature = msg.current_temp
+    minimum = msg.temperature_min
+    maximum = msg.temperature_max
+    
+    # Check if the current temperature is below the minimum
+    if current_temperature < minimum:
+        await ctx.send(sender, AlertResponse(alert_status=f"Temperature is below the preferred range {minimum}°C."))
+    
+    # Check if the current temperature is above the maximum
+    if current_temperature > maximum:
+        await ctx.send(sender, AlertResponse(alert_status=f"Temperature is above the preferred range {maximum}°C."))
